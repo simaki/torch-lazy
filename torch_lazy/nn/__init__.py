7cae99cbf4a71847f2067c8f80007f5994a9e22d
Shota Imaki <shota.imaki.0801@gmail.com>
2021-04-04 16:39:25 +0900

    ENH: Create LazyBatchNorm (close #5 close #9)

--- torch_lazy/nn/__init__.py
+++ torch_lazy/nn/__init__.py
@@ -4,3 +4,7 @@ from .modules.linear import LazyBilinear
 from .modules.mlp import MLP
 from .modules.mlp import LazyMLP
 from .modules.normalization import LazyLayerNorm
+from .modules.normalization import LazyBatchNorm
+from .modules.normalization import LazyBatchNorm1d
+from .modules.normalization import LazyBatchNorm2d
+from .modules.normalization import LazyBatchNorm3d
