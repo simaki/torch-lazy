12fa5c22ad35602b024c4ce19071b96d88e0fbc1
Shota Ray Imaki <shota.imaki.0801@gmail.com>
2021-04-07 21:00:09 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,6 +3,5 @@
 python3 -m pytest --doctest-modules torch_lazy
 python3 -m pytest --doctest-modules tests
 
-python3 -m flake8 torch_lazy
 python3 -m black --check torch_lazy || read -p "Run formatter? (y/N): " yn; [[ $yn = [yY] ]] && python3 -m black torch_lazy
 python3 -m isort --check --force-single-line-imports torch_lazy || read -p "Run formatter? (y/N): " yn; [[ $yn = [yY] ]] && python3 -m isort --force-single-line-imports torch_lazy
